ca76a9f943c9aeb5933d059c43687380760e3116
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-03-16 15:05:26 +0200

    Listings Model | RealTor Model

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class RealTor(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    is_mvp = models.BooleanField(default=True)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
